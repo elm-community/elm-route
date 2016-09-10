1e691dc0231f3f692adab1baf28ece6ed2091407
Bogdan Popa <popa.bogdanp@gmail.com>
2016-09-10 12:09:16 +0300

    upgrade elm-test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,9 +2,11 @@ module Tests exposing (..)
 
 import Combine
 import Combine.Infix exposing ((<$))
-import ElmTest exposing (..)
+import Expect
+import Fuzz exposing (Fuzzer, tuple, tuple3)
 import Route exposing (..)
 import String
+import Test exposing (..)
 
 
 type Sitemap
@@ -64,8 +66,8 @@ customR =
     Custom' := static "custom" </> custom fooP
 
 
-siteMap : Router Sitemap
-siteMap =
+routes : Router Sitemap
+routes =
     router
         [ homeR
         , usersR
@@ -102,69 +104,125 @@ render r =
             reverse customR [ toString x ]
 
 
+ints1 : Fuzzer Int
+ints1 =
+    Fuzz.int
+
+
+ints2 : Fuzzer ( Int, Int )
+ints2 =
+    tuple ( Fuzz.int, Fuzz.int )
+
+
+ints3 : Fuzzer ( Int, Int, Int )
+ints3 =
+    tuple3 ( Fuzz.int, Fuzz.int, Fuzz.int )
+
+
 matching : Test
 matching =
-    suite "Matching"
-        [ test "Match home" <|
-            assertEqual (Just Home)
-                (match siteMap "/")
-        , test "Match users" <|
-            assertEqual (Just Users)
-                (match siteMap "/users")
-        , test "Match user" <|
-            assertEqual (Just (User 1))
-                (match siteMap "/users/1")
-        , test "Fail user" <|
-            assertEqual Nothing
-                (match siteMap "/users/a")
-        , test "Match user emails" <|
-            assertEqual (Just (UserEmails 1))
-                (match siteMap "/users/1/emails")
-        , test "Match user email" <|
-            assertEqual (Just (UserEmail 1 1))
-                (match siteMap "/users/1/emails/1")
-        , test "Match deep" <|
-            assertEqual (Just (Deep 1 2 3))
-                (match siteMap "/deep/1/2/3")
-        , test "Match custom Foo" <|
-            assertEqual (Just (Custom' Foo))
-                (match siteMap "/custom/Foo")
-        , test "Match custom Bar" <|
-            assertEqual (Just (Custom' Bar))
-                (match siteMap "/custom/Bar")
-        , test "Fail custom" <|
-            assertEqual Nothing
-                (match siteMap "/custom/Baz")
-        , test "Not found" <|
-            assertEqual Nothing
-                (match siteMap "/i-dont-exist")
-        ]
+    let
+        equal x path _ =
+            Expect.equal x (match routes path)
+
+        matches =
+            Just >> equal
+
+        matches' x path =
+            matches x path ()
+
+        fails =
+            equal Nothing
+    in
+        describe "match"
+            [ test
+                "fails to match on parse failure"
+                (fails "/users/a")
+            , test
+                "fails to match on custom parse failure"
+                (fails "/custom/abc")
+            , test
+                "fails to match nonexistent paths"
+                (fails "/i-dont-exist")
+            , test
+                "matches the root path"
+                (matches Home "/")
+            , test
+                "matches static paths"
+                (matches Users "/users")
+            , test
+                "matches custom ADT routes"
+                (matches (Custom' Foo) "/custom/Foo")
+            , test
+                "matches custom ADT routes"
+                (matches (Custom' Bar) "/custom/Bar")
+            , fuzz ints1
+                "matches one dynamic segment"
+                (\x -> matches' (User x) ("/users/" ++ toString x))
+            , fuzz ints1
+                "matches one suffixed dynamic segment"
+                (\x -> matches' (UserEmails x) ("/users/" ++ toString x ++ "/emails"))
+            , fuzz ints2
+                "matches two dynamic segments around a static segment"
+                (\( x, y ) -> matches' (UserEmail x y) ("/users/" ++ toString x ++ "/emails/" ++ toString y))
+            , fuzz ints3
+                "matches many dynamic segments"
+                (\( x, y, z ) -> matches' (Deep x y z) ("/deep/" ++ String.join "/" (List.map toString [ x, y, z ])))
+            ]
 
 
 reversing : Test
 reversing =
-    suite "Reversing"
-        [ test "Reverse home" (assertEqual "/" (reverse homeR []))
-        , test "Reverse user" (assertEqual "/users/1" (reverse userR [ "1" ]))
-        , test "Reverse custom" (assertEqual "/custom/Foo" (reverse customR [ "Foo" ]))
-        ]
+    let
+        compare parts route params _ =
+            Expect.equal
+                ("/" ++ String.join "/" parts)
+                (reverse route params)
+    in
+        describe "reverse"
+            [ test
+                "reverses the root route"
+                (compare [] homeR [])
+            , test
+                "reverses custom routes"
+                (\x -> compare [ "custom", "Foo" ] customR [ "Foo" ] ())
+            , fuzz ints1
+                "reverses dynamic routes"
+                (\x -> compare [ "users", toString x ] userR [ toString x ] ())
+            ]
 
 
 rendering : Test
 rendering =
-    suite "Reversing Safely"
-        [ test "Reverse home" (assertEqual "/" (render (Home)))
-        , test "Reverse users" (assertEqual "/users" (render (Users)))
-        , test "Reverse user" (assertEqual "/users/1" (render (User 1)))
-        , test "Reverse deep" (assertEqual "/deep/1/2/3" (render (Deep 1 2 3)))
-        , test "Reverse custom" (assertEqual "/custom/Foo" (render (Custom' Foo)))
-        ]
+    let
+        compare parts route _ =
+            Expect.equal
+                ("/" ++ String.join "/" parts)
+                (render route)
+    in
+        describe "render"
+            [ test
+                "renders the root route"
+                (compare [] Home)
+            , test
+                "renders static routes"
+                (compare [ "users" ] Users)
+            , test
+                "renders custom parser routes"
+                (compare [ "custom", "Foo" ] (Custom' Foo))
+            , fuzz ints1
+                "renders dynamic routes"
+                (\x -> compare [ "users", toString x ] (User x) ())
+            , fuzz ints3
+                "renders deep dynamic routes"
+                (\( x, y, z ) -> compare [ "deep", toString x, toString y, toString z ] (Deep x y z) ())
+            ]
 
 
 all : Test
 all =
-    suite "Tests" [ matching, reversing, rendering ]
-
-
-main =
-    runSuite all
+    describe "elm-route"
+        [ matching
+        , reversing
+        , rendering
+        ]
